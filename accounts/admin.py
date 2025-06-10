472dd73e6c13f7cc2921a7d0ee0a437d943b0486
Aurimas <ransys.dev@gmail.com>
2025-06-10 17:46:09 +0100

    created user profiles template, added admin registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'user_type', 'company_name', 'hourly_rate', 'created_at']
+    list_filter = ['user_type', 'created_at']
+    search_fields = ['user__username', 'user__email', 'company_name', 'skills']
+    readonly_fields = ['created_at']
+    
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user', 'user_type')
+        }),
+        ('Profile Details', {
+            'fields': ('bio', 'skills', 'phone')
+        }),
+        ('Work Information', {
+            'fields': ('hourly_rate', 'company_name')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at',)
+        }),
+    )
