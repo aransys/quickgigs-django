d929b0e34d2e0eb9e7577ca1a7bebf55063d7fda
Aurimas <80128260+aransys@users.noreply.github.com>
2025-08-24 16:15:52 +0100

    Fix missing import in wsgi.py - add get_wsgi_application import

--- quickgigs_project/wsgi.py
+++ quickgigs_project/wsgi.py
@@ -11,6 +11,7 @@ import os
 import sys
 import django
 import subprocess
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quickgigs_project.settings')
 
